11bea7384a7d4b3dda9b3e17026ef1e062522a37
digambar1411 <deshawaldigambar@gmail.com>
2026-01-11 14:23:50 +0530

    day 1 variables

---
+++ 1_day_variable_datatypes.py
@@ -0,0 +1,11 @@
+print('first','second','third', sep='\n')
+print('new output', end=' & ')
+
+# default value
+# sep = ' '
+# end = '\n'
+
+# Datatypes 
+# Primitive - int, float, str, boolean, None 
+# Non-primitive - list, set, dict, tupple 
+# Used Defined - linkedlist, stack, queue, graph, tree 
