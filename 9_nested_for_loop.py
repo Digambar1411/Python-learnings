97e9b0a94ac833224fa0ba294a320d1c6627aae3
digambar1411 <deshawaldigambar@gmail.com>
2026-01-12 15:43:47 +0530

    day 9, nested for loop, chr, ord

---
+++ 9_nested_for_loop.py
@@ -0,0 +1,21 @@
+for i in range(3,5,1):
+  for j in range(10,8,-1):
+    for k in range(15,21,2):
+      print(i,j,k)
+    print('k loop is break')
+  print('j loop is break')  
+print('i loop is break')
+
+for i in range(3,5,1):
+  for j in range(10,8,-1):
+    print('JBL',j)
+  for k in range(15,21,2):
+    print('Boult',k)
+
+
+
+print(chr(65))
+print(chr(97))
+
+print(ord('A'))
+print(ord('p'))
